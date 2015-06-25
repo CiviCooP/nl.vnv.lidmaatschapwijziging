84164cf77156858f22995bbdbf3ad859f68661f2
Jan-Derek <j.vos@bosqom.nl>
2015-06-25 12:18:54 +0200

    First commit

---
+++ CRM/Lidmaatschapwijziging/ConfigContact.php
@@ -0,0 +1,352 @@
+<?php
+/**
+ * Class following Singleton pattern for specific extension configuration
+ *
+ * @author Jan-Derek (CiviCooP) <j.vos@bosqom.nl>
+ */
+class CRM_Lidmaatschapwijziging_ConfigContact {
+  
+  /*
+   * singleton pattern
+   */
+  static private $_singleton = NULL;
+  
+  // Contact
+  protected $contact_id = 0;
+  protected $contact = array();
+
+  // Vnv Info
+  protected $vnvInfoCustomGroupName = 'VNV_info';
+  protected $vnvInfoCustomGroup = array();
+  protected $vnvInfoCustomFields = array();
+  protected $vnvInfoCustomFieldsOptionValues = array();
+  protected $vnvInfoCustomValues = array();
+  
+  // Werkgever
+  protected $werkgeverCustomGroupName = 'Werkgever';
+  protected $werkgeverCustomGroup = array();
+  protected $werkgeverCustomFields = array();
+  protected $werkgeverCustomFieldsOptionValues = array();
+  protected $werkgeverCustomValues = array();
+
+  /**
+   * Constructor
+   */
+  function __construct($contact_id) {
+    // Contact
+    $this->setContact($contact_id);
+    
+    // Vnv Info
+    $this->setVnvInfoCustomGroup();
+    $this->setVnvInfoCustomFields();
+    $this->setvnvInfoCustomFieldsOptionValues();
+    $this->setVnvInfoCustomValues();
+    
+    // Werkgever
+    $this->setWerkgeverCustomGroup();
+    $this->setWerkgeverCustomFields();
+    $this->setWerkgeverCustomFieldsOptionValues();
+    $this->setWerkgeverCustomValues();
+  }
+  
+  /**
+   * Function to return singleton object
+   * 
+   * @return object $_singleton
+   * @access public
+   * @static
+   */
+  public static function &singleton($contact_id) {
+    if (self::$_singleton === NULL) {
+      self::$_singleton = new CRM_Lidmaatschapwijziging_Config($contact_id);
+    }
+    return self::$_singleton;
+  }
+  
+  // Contact
+  protected function setContact($contact_id){
+    if(empty($contact_id)){
+      echo('No contact id, '
+        . 'error from function setContact');
+        CRM_Utils_System::civiExit();
+    }
+    
+    $this->contact_id = $contact_id;
+    
+    /*try {
+      $params = array(
+        'version' => 3,
+        'sequential' => 1,
+        'contact_id' => $this->contact_id,
+      );
+      $this->contact = civicrm_api('Contact', 'getsingle', $params);
+      
+    } catch (CiviCRM_API3_Exception $ex) {
+      throw new Exception('Could not find contact, '
+        . 'error from API Contact getsingle: '.$ex->getMessage());
+    }*/
+    
+    // the source or contact source does not exist if youn use the api
+    try {
+      $query = 'SELECT * FROM civicrm_contact WHERE id= %1';
+      $params = array(1 => array($this->contact_id, 'Positive'));
+      $dao = CRM_Core_DAO::executeQuery($query, $params);
+            
+      if ($dao->fetch()) {
+        foreach($dao as $field => $value){
+          $this->contact[$field] = $value;
+        }
+      }else {
+        echo('No contact values, '
+        . 'error from function setContact');
+        CRM_Utils_System::civiExit();
+      }  
+    } catch (CiviCRM_API3_Exception $ex) {
+      throw new Exception('Could not find contact, '
+        . 'error from CRM_Core_DAO: '.$ex->getMessage());
+    }
+  }
+  
+  public function getContact() {
+    return $this->contact;
+  }
+
+
+  // Vnv Info 
+  // Vnv Info - Custom Group
+  protected function setVnvInfoCustomGroup(){
+    try {
+      $params = array(
+        'version' => 3,
+        'sequential' => 1,
+        'name' => $this->vnvInfoCustomGroupName,
+      );
+      $this->vnvInfoCustomGroup = civicrm_api('CustomGroup', 'getsingle', $params);
+      
+    } catch (CiviCRM_API3_Exception $ex) {
+      throw new Exception('Could not find vnv info custom group id, '
+        . 'error from API CustomGroup getvalue: '.$ex->getMessage());
+    }
+  }
+  
+  public function getVnvInfoCustomGroup(){
+    return $this->vnvInfoCustomGroup;
+  }
+  
+  public function getVnvInfoCustomGroupField($field){
+    return $this->vnvInfoCustomGroup[$field];
+  }
+  
+  // Vnv Info - Custom Fields
+  protected function setVnvInfoCustomFields(){
+    try {
+      $params = array(
+        'version' => 3,
+        'sequential' => 1,
+        'custom_group_id' => $this->getVnvInfoCustomGroupField('id'),
+      );
+      $result = civicrm_api('CustomField', 'get', $params);
+      
+      foreach ($result['values'] as $key => $value){
+        $this->vnvInfoCustomFields[$value['id']] = $value;
+      }
+      
+    } catch (CiviCRM_API3_Exception $ex) {
+      throw new Exception('Could not find vnv info custom fields, '
+        . 'error from API CustomField get: '.$ex->getMessage());
+    }
+  }
+  
+  public function getVnvInfoCustomFields(){
+    return $this->vnvInfoCustomFields;
+  }
+  
+  public function getVnvInfoCustomFieldsByName(){
+    $return = array();
+    foreach ($this->vnvInfoCustomFields as $id => $field){
+      $return[$field['name']] = $field;
+    }
+    
+    return $return;
+  }
+  
+  // Vnv Info - Custom Fields Options Values
+  protected function setVnvinfoCustomFieldsOptionValues(){
+    foreach($this->vnvInfoCustomFields as $id => $field){
+      if(isset($field['option_group_id']) and !empty($field['option_group_id'])){
+        $this->vnvInfoCustomFieldsOptionValues[$field['name']] = array();
+        
+        try {
+          $params = array(
+            'version' => 3,
+            'sequential' => 1,
+            'option_group_id' => $field['option_group_id'],
+          );
+          $result = civicrm_api('OptionValue', 'get', $params);
+
+          foreach ($result['values'] as $key => $value){
+            $this->vnvInfoCustomFieldsOptionValues[$field['name']][$value['name']] = $value;
+          }
+
+        } catch (CiviCRM_API3_Exception $ex) {
+          throw new Exception('Could not find vnv info custom fields option values, '
+            . 'error from API CustomField get: '.$ex->getMessage());
+        }
+      }
+    }
+  }
+    
+  public function getVnvInfoCustomFieldsOptionValues(){
+    return $this->vnvInfoCustomFieldsOptionValues;
+  }
+    
+  // Vnv Info - Custom Values
+  protected function setVnvInfoCustomValues(){
+    try {
+      $query = 'SELECT * FROM '.$this->getVnvInfoCustomGroupField('table_name').' WHERE entity_id= %1';
+      $params = array(1 => array($this->contact_id, 'Positive'));
+      $dao = CRM_Core_DAO::executeQuery($query, $params);
+            
+      if ($dao->fetch()) {
+        foreach($dao as $field => $value){
+          $this->vnvInfoCustomValues[$field] = $value;
+        }
+      }else {
+        echo('No custom values, '
+        . 'error from function setVnvInfoCustomValues');
+        CRM_Utils_System::civiExit();
+      }  
+    } catch (CiviCRM_API3_Exception $ex) {
+      throw new Exception('Could not find vnv info custom values, '
+        . 'error from CRM_Core_DAO: '.$ex->getMessage());
+    }
+  }
+  
+  public function getVnvInfoCustomValues(){
+    return $this->vnvInfoCustomValues;
+  }
+  
+  public function getVnvInfoCustomValuesField($field){
+    return $this->vnvInfoCustomValues[$field];
+  }
+  
+  // Werkgever 
+  // Werkgever - Custom Group
+  protected function setWerkgeverCustomGroup(){
+    try {
+      $params = array(
+        'version' => 3,
+        'sequential' => 1,
+        'name' => $this->werkgeverCustomGroupName,
+      );
+      $this->werkgeverCustomGroup = civicrm_api('CustomGroup', 'getsingle', $params);
+      
+    } catch (CiviCRM_API3_Exception $ex) {
+      throw new Exception('Could not find werkgever custom group id, '
+        . 'error from API CustomGroup getvalue: '.$ex->getMessage());
+    }
+  }
+  
+  public function getWerkgeverCustomGroup(){
+    return $this->werkgeverCustomGroup;
+  }
+  
+  public function getWerkgeverCustomGroupField($field){
+    return $this->werkgeverCustomGroup[$field];
+  }
+  
+  // Werkgever - Custom Fields
+  protected function setWerkgeverCustomFields(){    
+    try {
+      $params = array(
+        'version' => 3,
+        'sequential' => 1,
+        'custom_group_id' => $this->getWerkgeverCustomGroupField('id'),
+      );
+      $result = civicrm_api('CustomField', 'get', $params);
+            
+      foreach ($result['values'] as $key => $value){
+        $this->werkgeverCustomFields[$key] = $value;
+      }
+      
+      
+      
+    } catch (CiviCRM_API3_Exception $ex) {
+      throw new Exception('Could not find werkgever custom fields, '
+        . 'error from API CustomField get: '.$ex->getMessage());
+    }
+  }
+  
+  public function getWerkgeverCustomFields(){
+    return $this->werkgeverCustomFields;
+  }
+  
+  public function getWerkgeverCustomFieldsByName(){    
+    $return = array();
+    foreach ($this->werkgeverCustomFields as $key => $value){
+      $return[$value['name']] = $value;
+    }
+    
+    return $return;
+  }
+  
+  // Werkgever - Custom Fields Options Values
+  protected function setWerkgeverCustomFieldsOptionValues(){
+    foreach($this->werkgeverCustomFields as $id => $field){
+      if(isset($field['option_group_id']) and !empty($field['option_group_id'])){
+        $this->werkgeverCustomFieldsOptionValues[$field['name']] = array();
+        
+        try {
+          $params = array(
+            'version' => 3,
+            'sequential' => 1,
+            'option_group_id' => $field['option_group_id'],
+          );
+          $result = civicrm_api('OptionValue', 'get', $params);
+
+          foreach ($result['values'] as $key => $value){
+            $this->werkgeverCustomFieldsOptionValues[$field['name']][$value['name']] = $value;
+          }
+
+        } catch (CiviCRM_API3_Exception $ex) {
+          throw new Exception('Could not find werkgever custom fields option values, '
+            . 'error from API CustomField get: '.$ex->getMessage());
+        }
+      }
+    }
+  }
+    
+  public function getWerkgeverCustomFieldsOptionValues(){
+    return $this->werkgeverCustomFieldsOptionValues;
+  }
+  
+  // Werkgever - Custom Values
+  protected function setWerkgeverCustomValues(){
+    try {
+      $query = 'SELECT * FROM '.$this->getWerkgeverCustomGroupField('table_name').' WHERE entity_id= %1';
+      $params = array(1 => array($this->contact_id, 'Positive'));
+      $dao = CRM_Core_DAO::executeQuery($query, $params);
+            
+      if ($dao->fetch()) {
+        foreach($dao as $field => $value){
+          $this->werkgeverCustomValues[$field] = $value;
+        }
+      }else {
+        echo('No custom values, '
+        . 'error from function setWerkgeverCustomValues');
+        CRM_Utils_System::civiExit();
+      }  
+    } catch (CiviCRM_API3_Exception $ex) {
+      throw new Exception('Could not find werkgever custom values, '
+        . 'error from CRM_Core_DAO: '.$ex->getMessage());
+    }
+  }
+  
+  public function getWerkgeverCustomValues(){
+    return $this->werkgeverCustomValues;
+  }
+  
+  public function getWerkgeverCustomValuesField($field){
+    return $this->werkgeverCustomValues[$field];
+  }
+}
---
+++ CRM/Lidmaatschapwijziging/Form/LidmaatschapWijzigingContact.php
@@ -0,0 +1,255 @@
+<?php
+
+require_once 'CRM/Core/Form.php';
+
+/**
+ * Form controller class
+ *
+ * @see http://wiki.civicrm.org/confluence/display/CRMDOC43/QuickForm+Reference
+ */
+class CRM_Lidmaatschapwijziging_Form_LidmaatschapWijzigingContact extends CRM_Core_Form {
+  public $_contactId;
+  public $_display_name;
+  
+  public $_vnvinfoId;
+  public $_werkgeverId;
+  
+  public $_values = array();
+  
+  /**
+   * This function is called prior to building and submitting the form
+   */
+  function preProcess() {  
+    // check contact_id
+    $this->_contactId = CRM_Utils_Request::retrieve('cid', 'Positive', $this);
+    if(empty($this->_contactId)){
+      CRM_Core_Error::statusBounce(ts('Could not get a contact id.'), NULL, ts('Lidmaatschap Wijziging - Contact')); // this also redirects to the default civicrm page
+    }
+    
+    // check for permissions
+    $session = CRM_Core_Session::singleton();
+    if (!CRM_Contact_BAO_Contact_Permission::allow($this->_contactId, CRM_Core_Permission::EDIT)) {
+      CRM_Core_Error::statusBounce(ts('You do not have the necessary permission to edit this contact.'), NULL, ts('Lidmaatschap Wijziging - Contact')); // this also redirects to the default civicrm page
+    }
+    
+    // redirect user after postProcess
+    $urlParams = 'reset=1&cid=' . $this->_contactId;
+    $session->pushUserContext(CRM_Utils_System::url('civicrm/lidmaatschapwijziging/contact', $urlParams));
+    
+    // get values
+    $config = CRM_Lidmaatschapwijziging_ConfigContact::singleton($this->_contactId);
+    $this->_values = $config->getContact();
+    
+    // set display name
+    $this->_display_name = $this->_values['display_name'];
+    
+    // set title
+    CRM_Utils_System::setTitle('LidmaatschapWijziging - Contact - ' . $this->_values['display_name']);
+    
+    // set contact id
+    $this->_values['contact_id'] = $this->_contactId;
+        
+    // change the default name like huppeldepup_35 to huppeldepup, this
+    // ensures the we can use the know names for custom fields in the template like
+    // huppeldepup and not the column_names like huppeldepup_35
+    $values = $config->getVnvInfoCustomValues();
+    
+    // set vnvn info id, is neede for update or insert in the postProccess
+    if(isset($values['id']) and !empty($values['id'])){
+      $this->_vnvinfoId = $values['id'];
+    }
+    
+    foreach($config->getVnvInfoCustomFields() as $key => $field){
+      $this->_values[$field['name']] = $values[$field['column_name']];
+    }
+    
+    $values = $config->getWerkgeverCustomValues();
+    
+    // set werkgever id, is neede for update or insert in the postProccess
+    if(isset($values['id']) and !empty($values['id'])){
+      $this->_werkgeverId = $values['id'];
+    }
+    
+    foreach($config->getWerkgeverCustomFields() as $key => $field){
+      $this->_values[$field['name']] = $values[$field['column_name']];
+    }
+    
+    // assign values needed for the template
+    $this->assign('contactId', $this->_contactId);
+    $this->assign('employerDataURL', '/civicrm/ajax/rest?className=CRM_Contact_Page_AJAX&fnName=getContactList&json=1&context=contact&org=1&employee_id=' . $this->_contactId);
+    $this->assign('currentEmployer', $this->_values['employer_id']);
+  }
+  
+  /**
+   * This function is called prior to building and submitting the form and after the preProcess
+   */
+  function setDefaultValues() {
+    $defaults = array();
+    $defaults = $this->_values;
+    
+    // current employer id
+    $defaults['current_employer_id'] = $defaults['employer_id'];
+    
+    return $defaults;
+  }
+  
+  function buildQuickForm() {
+    $config = CRM_Lidmaatschapwijziging_ConfigContact::singleton($this->_contactId);
+    
+    // Contactdetails
+    $this->add('hidden', 'contact_id', ts('Contact id'), '', true);
+    $this->add('hidden', 'contact_type', ts('Contact id'), '', true);
+    $this->add('hidden', 'current_employer_id', ts('Current empoyer id'), '', true);
+    $this->add('text', 'current_employer',  ts('Current empoyer'), '', true );
+    $this->add('text', 'job_title', ts('Job title'), '', true );
+    $this->add('text', 'source', ts('Source'), '', true );
+    
+    // VNV info 
+    $vnvInfoFields = $config->getVnvInfoCustomFieldsByName();
+    $vnvInfoFieldsOptions = $config->getVnvInfoCustomFieldsOptionValues();
+    $vnvInfoFieldsNeeded = array('Lid_kandidaat_voldoet_aan_een_van_de_volgende_criteria', 'Lid_beroepsvereniging_voldoet_aan_een_van_de_volgende_criteria');
+        
+    foreach($vnvInfoFields as $name => $field){
+      
+      switch($name){
+        
+        case 'Lid_kandidaat_voldoet_aan_een_van_de_volgende_criteria':
+          $options = array();
+          foreach($vnvInfoFieldsOptions[$name] as $option_name => $option){
+            $options[$option['value']] = ts($option['label']);
+          }
+                    
+          $this->addRadio($name, ts($field['label']), $options, NULL, '<br/>', true);
+          break;
+          
+        case 'Lid_beroepsvereniging_voldoet_aan_een_van_de_volgende_criteria':
+          $options = array();
+          foreach($vnvInfoFieldsOptions[$name] as $option_name => $option){
+            $options[$option['value']] = ts($option['label']);
+          }
+                    
+          $this->addRadio($name, ts($field['label']), $options, NULL, '<br/>', true);
+          break;
+      }
+    }
+    
+    // Werkgever
+    $werkgeverFields = $config->getWerkgeverCustomFieldsByName();
+    $werkgeverFieldsNeeded = array('Vl_type', 'Datum_in_dienst', 'Pers_code_let_', 'Postvak', 'persnr', 'Standplaats_Werkgever');
+    
+    foreach($werkgeverFields as $name => $field){
+      switch($name){
+        case 'Vl_type':
+          $this->add('text', $name, ts($field['label']), '', true);
+          break;
+        case 'Datum_in_dienst':
+          $this->addDate($name, ts($field['label']), true);
+          break;
+        case 'Pers_code_let_':
+          $this->add('text', $name, ts($field['label']), '', true);
+          break;
+        case 'Postvak':
+          $this->add('text', $name, ts($field['label']), '', true);
+          break;
+        case 'persnr':
+          $this->add('text', $name, ts($field['label']), '', true);
+          break;
+        case 'Standplaats_Werkgever':
+          $this->add('text', $name, ts($field['label']), '', true);
+          break;
+      }
+    }
+        
+    $this->addButtons(array(
+      array(
+        'type' => 'submit',
+        'name' => ts('Submit'),
+        'isDefault' => TRUE,
+      ),
+    ));
+    
+    // export form elements
+    $this->assign('elementNames', $this->getRenderableElementNames());
+    parent::buildQuickForm();
+  }
+  
+  /**
+   * Get the fields/elements defined in this form.
+   *
+   * @return array (string)
+   */
+  function getRenderableElementNames() {
+    // The _elements list includes some items which should not be
+    // auto-rendered in the loop -- such as "qfKey" and "buttons".  These
+    // items don't have labels.  We'll identify renderable by filtering on
+    // the 'label'.
+    $elementNames = array();
+    foreach ($this->_elements as $element) {
+      $label = $element->getLabel();
+      if (!empty($label)) {
+        $elementNames[] = $element->getName();
+      }
+    }
+    return $elementNames;
+  }
+  
+  function postProcess() {
+    $values = $this->exportValues();
+        
+    $config = CRM_Lidmaatschapwijziging_ConfigContact::singleton($this->_contactId);
+        
+    // set current_employer
+    // current employer
+    if (is_numeric($values['current_employer_id']) && $values['current_employer']) {
+      $values['current_employer'] = $values['current_employer_id'];
+    }
+    
+    // api create contact
+    try {    
+      $params = array(
+        'version' => 3,
+        'sequential' => 1,
+        'id' => $this->_contactId,
+        'contact_type' => $values['contact_type'],
+        'current_employer' => $values['current_employer'],
+        'job_title' => $values['job_title'],
+        'source' => $values['source'],
+      );
+      
+      // for the custom values in contact, the need to be custom_35
+      foreach($config->getVnvInfoCustomFields() as $key => $field){
+        if(isset($values[$field['name']])){
+          $params['custom_' . $field['id']] = $values[$field['name']];
+        }
+      }
+        
+      foreach($config->getWerkgeverCustomFields() as $key => $field){
+        if(isset($values[$field['name']])){
+          $params['custom_' . $field['id']] = $values[$field['name']];
+        }
+      }
+            
+      $result = civicrm_api('Contact', 'create', $params);
+      
+      // set message
+      $session = CRM_Core_Session::singleton();
+      $session->setStatus(sprintf(ts('%s is saved !'), $this->_display_name), ts('Contact Saved'), 'success');
+      
+      // redirect user
+      $url = CRM_Utils_System::url('civicrm/lidmaatschapwijziging/contact', 'reset=1&cid=' . $this->_contactId);
+      CRM_Utils_System::redirect($url);
+      
+    } catch (CiviCRM_API3_Exception $ex) {
+      throw new Exception('Could not create contact, '
+        . 'error from Api Contact create: '.$ex->getMessage());
+    }   
+    
+    // set message
+    $session = CRM_Core_Session::singleton();
+    $session->setStatus(sprintf(ts('%s is not saved !'), $this->_display_name), ts('Contact Not Saved'), 'error');
+      
+    // redirect user
+    $url = CRM_Utils_System::url('civicrm/lidmaatschapwijziging/contact', 'reset=1&cid=' . $this->_contactId);
+    CRM_Utils_System::redirect($url);
+  }
+}
